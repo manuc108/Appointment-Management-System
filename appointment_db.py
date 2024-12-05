5cdbf8150b2a9dfc7853ff4f6690409e91cb2521
manuc108 <manu.v23csai@nst.rishihood.edu.in>
2024-12-05 12:15:22 +0530

    Update appointment-management-system

--- appointment_db.py
+++ appointment_db.py
@@ -1,6 +1,11 @@
+import os
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 Base = declarative_base()
 
@@ -13,10 +18,10 @@ class Appointment(Base):
     appointment_date = Column(DateTime, nullable=False)
     is_canceled = Column(Boolean, default=False)
 
-# Database setup
-DATABASE_URL = "sqlite:///./appointments.db"
+# Database setup 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Create tables
+# Create tables in the database
 Base.metadata.create_all(bind=engine)
+
